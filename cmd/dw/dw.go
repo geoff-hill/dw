ac905d4d0c5f09a9a3f1458817755766c01d2978
Geoff Hill <geoff.hill.au@gmail.com>
2023-05-07 09:07:05 +1000

    Package fix

--- cmd/dw/dw.go
+++ cmd/dw/dw.go
@@ -1,7 +1,11 @@
 package main
 
 import "fmt"
+<<<<<<< HEAD
 import "github.com/geoff-hill/dw/pkg/libgw"
+=======
+import "github.com/geoff-hill/dw/pkg/dw/libgw"
+>>>>>>> 160aae1 (Package fix)
 
 func main() {
     fmt.Println("hello world")
